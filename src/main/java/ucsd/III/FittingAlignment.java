e86ed4e9738027677c3245b33bd75d4d7ba6c247
Samuel Lee <slee@broadinstitute.org>
2015-08-09 00:02:57 -0400

    III week 2: added FittingAlignment

--- src/main/java/ucsd/III/FittingAlignment.java
+++ src/main/java/ucsd/III/FittingAlignment.java
@@ -1,38 +1,45 @@
 package ucsd.III;
 
 import ucsd.ConsoleCapturer;
-import ucsd.DataTableUtils;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class GlobalAlignment {
+public class FittingAlignment {
     private enum Action {
         INSERTION, DELETION, MATCH
     }
 
-    public static Action[][] getBacktrackMatrix(Map<Map.Entry, Integer> scoringMatrix, int indelPenalty,
-                                                String v, String w) {
+    private static int maxLength, iMax, jMax;
+
+    public static Action[][] getBacktrackMatrix(String v, String w) {
         Action[][] backtrackMatrix = new Action[v.length() + 1][w.length() + 1];
         int[][] lengthMatrix = new int[v.length() + 1][w.length() + 1];
 
         //initialize edges
         for (int i = 0; i <= v.length(); i++) {
-            lengthMatrix[i][0] = -indelPenalty*i;
+            lengthMatrix[i][0] = 0;
         }
         for (int j = 0; j <= w.length(); j++) {
-            lengthMatrix[0][j] = -indelPenalty*j;
+            lengthMatrix[0][j] = -j;
         }
         for (int i = 1; i <= v.length(); i++) {
             for (int j = 1; j <= w.length(); j++) {
                 List<Integer> scores = Arrays.asList(
-                        lengthMatrix[i - 1][j] - indelPenalty,
-                        lengthMatrix[i][j - 1] - indelPenalty,
-                        lengthMatrix[i - 1][j - 1]
-                                + scoringMatrix.get(new AbstractMap.SimpleEntry(v.charAt(i - 1), w.charAt(j - 1))));
+                        lengthMatrix[i - 1][j] - 1,
+                        lengthMatrix[i][j - 1] - 1,
+                        v.charAt(i - 1) == w.charAt(j - 1) ?
+                                lengthMatrix[i - 1][j - 1] + 1 : lengthMatrix[i - 1][j - 1] - 1);
                 lengthMatrix[i][j] = Collections.max(scores);
+                if (j == w.length() && lengthMatrix[i][j] >= maxLength) {
+                    maxLength = lengthMatrix[i][j];
+                    iMax = i;
+                    jMax = j;
+                }
                 if (lengthMatrix[i][j] ==  scores.get(0)) {
                     backtrackMatrix[i][j] = Action.DELETION;
                 } else if (lengthMatrix[i][j] == scores.get(1)) {
@@ -50,7 +57,7 @@ public class GlobalAlignment {
             return getActionsFromBacktrack(backtrackMatrix, i, j - 1) + " INS ";
         }
         if (i != 0 & j == 0) {
-            return getActionsFromBacktrack(backtrackMatrix, i - 1, j) + " DEL ";
+            return "FREE ";
         }
         if (i != 0 && j != 0) {
             Action action = backtrackMatrix[i][j];
@@ -66,15 +73,30 @@ public class GlobalAlignment {
         return "";
     }
 
+    public static int getScoreFromBacktrack(Action[][] backtrackMatrix, String v, String w, int i, int j) {
+        if (j == 0) {
+            return 0;
+        }
+        if (i == 0) {
+            return -j;
+        }
+        Action action = backtrackMatrix[i][j];
+        switch (action) {
+            case DELETION:
+                return getScoreFromBacktrack(backtrackMatrix, v, w, i - 1, j) - 1;
+            case INSERTION:
+                return getScoreFromBacktrack(backtrackMatrix, v, w, i, j - 1) - 1;
+            case MATCH:
+                return v.charAt(i - 1) == w.charAt(j - 1) ?
+                        getScoreFromBacktrack(backtrackMatrix, v, w, i - 1, j - 1) + 1 :
+                        getScoreFromBacktrack(backtrackMatrix, v, w, i - 1, j - 1) - 1;
+        }
+        return 0;
+    }
+
     public static String getAlignmentFromBacktrack(Action[][] backtrackMatrix, String s, int i, int j,
                                                    boolean isReference) {
         if (isReference) {
-            if (i == 0 & j != 0) {
-                return getAlignmentFromBacktrack(backtrackMatrix, s, i, j - 1, isReference) + "-";
-            }
-            if (i != 0 & j == 0) {
-                return getAlignmentFromBacktrack(backtrackMatrix, s, i - 1, j, isReference) + s.charAt(i - 1);
-            }
             if (i != 0 && j != 0) {
                 Action action = backtrackMatrix[i][j];
                 switch (action) {
@@ -87,12 +109,6 @@ public class GlobalAlignment {
                 }
             }
         } else {
-            if (i == 0 & j != 0) {
-                return getAlignmentFromBacktrack(backtrackMatrix, s, i, j - 1, isReference) + s.charAt(j - 1);
-            }
-            if (i != 0 & j == 0) {
-                return getAlignmentFromBacktrack(backtrackMatrix, s, i - 1, j, isReference) + "-";
-            }
             if (i != 0 && j != 0) {
                 Action action = backtrackMatrix[i][j];
                 switch (action) {
@@ -106,32 +122,6 @@ public class GlobalAlignment {
             }
         }
         return "";
-    }
-
-    public static int getScoreFromBacktrack(Action[][] backtrackMatrix, Map<Map.Entry, Integer> scoringMatrix,
-                                            int indelPenalty, String v, String w, int i, int j) {
-        if (i == 0 && j == 0) {
-            return 0;
-        }
-        if (i == 0) {
-            return -indelPenalty*j;
-        }
-        if (j == 0) {
-            return -indelPenalty*i;
-        }
-        Action action = backtrackMatrix[i][j];
-        switch (action) {
-            case DELETION:
-                return getScoreFromBacktrack(backtrackMatrix, scoringMatrix, indelPenalty, v, w, i - 1, j)
-                        - indelPenalty;
-            case INSERTION:
-                return getScoreFromBacktrack(backtrackMatrix, scoringMatrix, indelPenalty, v, w, i, j - 1)
-                        - indelPenalty;
-            case MATCH:
-                return getScoreFromBacktrack(backtrackMatrix, scoringMatrix, indelPenalty, v, w, i - 1, j - 1)
-                        + scoringMatrix.get(new AbstractMap.SimpleEntry(v.charAt(i - 1), w.charAt(j - 1)));
-            default: return 0;
-        }
     }
 
     /**
@@ -154,20 +144,14 @@ public class GlobalAlignment {
             String v = br.readLine();
             String w = br.readLine();
 
-            Map<Map.Entry, Integer> scoringMatrix = DataTableUtils
-                    .getScoringMatrix("/home/slee/working/ucsd-bioinformatics/src/test/resources/BLOSUM62.txt");
-
-            int indelPenalty = 5;
-
-            Action[][] backtrackMatrix = getBacktrackMatrix(scoringMatrix, indelPenalty, v, w);
+            Action[][] backtrackMatrix = getBacktrackMatrix(v, w);
 
 //            printMatrix(backtrackMatrix);
-//            System.out.println(getActionsFromBacktrack(backtrackMatrix, v.length(), w.length()));
+//            System.out.println(getActionsFromBacktrack(backtrackMatrix, iMax, jMax));
 
-            int score = getScoreFromBacktrack(backtrackMatrix, scoringMatrix, indelPenalty,
-                    v, w, v.length(), w.length());
-            String vAlign = getAlignmentFromBacktrack(backtrackMatrix, v, v.length(), w.length(), true);
-            String wAlign = getAlignmentFromBacktrack(backtrackMatrix, w, v.length(), w.length(), false);
+            int score = getScoreFromBacktrack(backtrackMatrix, v, w, iMax, jMax);
+            String vAlign = getAlignmentFromBacktrack(backtrackMatrix, v, iMax, jMax, true);
+            String wAlign = getAlignmentFromBacktrack(backtrackMatrix, w, iMax, jMax, false);
 
             return ConsoleCapturer.toString(score + "\n" + vAlign + "\n" + wAlign);
         } catch (IOException e) {
@@ -176,7 +160,7 @@ public class GlobalAlignment {
     }
 
     public static void main(String[] args) throws IOException {
-        String result = doWork("src/test/resources/III/dataset_247_3.txt");
+        String result = doWork("src/test/resources/III/dataset_248_5.txt");
         System.out.println(result);
     }
 }
---
+++ src/test/java/ucsd/III/FittingAlignmentTest.java
@@ -0,0 +1,23 @@
+package ucsd.III;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FittingAlignmentTest {
+    @Test
+    public void test() {
+        String result = FittingAlignment.doWork("src/test/resources/III/sample/FittingAlignment.txt").split("\\n")[0];
+        String expected = "2";
+        //alignments might not match
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testExtra() {
+        String result = FittingAlignment.doWork("src/test/resources/III/sample/FittingAlignmentExtra.txt").split("\\n")[0];
+        String expected = "22";
+        //alignments might not match
+        Assert.assertEquals(result, expected);
+    }
+}
+
