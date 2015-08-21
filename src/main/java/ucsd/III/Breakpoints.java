3c0c636f7faefbaeb226086aed550b43226e22d7
Samuel Lee <slee@broadinstitute.org>
2015-08-20 22:30:40 -0400

    III week 4: Breakpoints

---
+++ src/main/java/ucsd/III/Breakpoints.java
@@ -0,0 +1,49 @@
+package ucsd.III;
+
+import ucsd.ConsoleCapturer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Breakpoints {
+    public static int getNumberOfAdjacencies(List<Integer> permutation) {
+        int numberOfAdjacencies = 0;
+        for (int i = 0; i < permutation.size() - 1; i++) {
+            if (permutation.get(i + 1) - permutation.get(i) == 1) {
+                numberOfAdjacencies++;
+            }
+        }
+        return numberOfAdjacencies;
+    }
+
+    public static int getNumberOfBreakpoints(int numberOfAdjacencies, int numberOfElements) {
+        return numberOfElements + 1 - numberOfAdjacencies;
+    }
+
+    public static String doWork(String dataFileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(dataFileName))) {
+            String[] permutationStrings = br.readLine().replaceAll("\\(|\\)", "").split("\\s+");
+            List<Integer> permutation = Arrays.stream(permutationStrings).map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            permutation.add(0, 0);
+            permutation.add(permutation.size(), permutation.size());
+            int numberOfAdjacencies = getNumberOfAdjacencies(permutation);
+            int numberOfBreakpoints = getNumberOfBreakpoints(numberOfAdjacencies, permutation.size() - 2);
+
+            return ConsoleCapturer.toString(numberOfBreakpoints);
+        } catch (IOException e) {
+            throw new RuntimeException("Input file not found.");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String result = doWork("src/test/resources/III/dataset_287_5.txt");
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/ucsd/III/BreakpointsTest.java
@@ -0,0 +1,21 @@
+package ucsd.III;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BreakpointsTest {
+    @Test
+    public void test() {
+        String result = Breakpoints.doWork("src/test/resources/III/sample/Breakpoints.txt");
+        String expected = "8";
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testExtra() {
+        String result = Breakpoints.doWork("src/test/resources/III/sample/BreakpointsExtra.txt");
+        String expected = "178";
+        Assert.assertEquals(result, expected);
+    }
+}
+
