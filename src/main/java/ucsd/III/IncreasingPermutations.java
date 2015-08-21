19e6dd6dff3df11fd31aa3a08864bc2124de95f7
Samuel Lee <slee@broadinstitute.org>
2015-08-21 17:00:20 -0400

    III week 4: updated IncreasingPermutations

--- src/main/java/ucsd/III/IncreasingPermutations.java
+++ src/main/java/ucsd/III/IncreasingPermutations.java
@@ -11,13 +11,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IncreasingPermutations {
-    public static int count = -1;
+    public static int count = 0;
+    public static final int n = 3;
+    public static final String format = "%" + n + "s";
 
     public static boolean isIncreasing(List<Integer> permutation) {
-        for (int i = 0; i < permutation.size() - 1; i++) {
-            if (permutation.get(i) == permutation.get(i + 1) + 1) {
+        for (int i = 0; i < n - 1; i++) {
+            final int first = permutation.get(i);
+            final int second = permutation.get(i + 1);
+            if ((first > 0 && second > 0 && first == second + 1) || (first < 0 && second < 0 && first == second - 1)) {
                 return false;
             }
         }
@@ -25,11 +30,11 @@ public class IncreasingPermutations {
     }
 
     public static List<List<Integer>> getSigned(List<Integer> permutation) {
-        List<List<Integer>> signedPermutations = new ArrayList<>();
-        String format = "%" + permutation.size() + "s";
-        for (int i = 0; i < Math.pow(2, permutation.size()); i++) {
+        final List<List<Integer>> signedPermutations = new ArrayList<>();
+
+        for (int i = 0; i < Math.pow(2, n); i++) {
             String iBinary = String.format(format, Integer.toBinaryString(i)).replace(' ', '0');
-            signedPermutations.add(IntStream.range(0, permutation.size()).boxed()
+            signedPermutations.add(IntStream.range(0, n).boxed()
                     .map(j -> permutation.get(j) * ((int) Math.signum(2 * Integer.parseInt(Character.toString(iBinary.charAt(j))) - 1)))
                     .collect(Collectors.toList()));
         }
@@ -38,10 +43,10 @@ public class IncreasingPermutations {
 
     public static void generate(int n, List<Integer> permutation) {
         if (n == 1) {
-            List<List<Integer>> signedPermutations = getSigned(permutation);
+            final List<List<Integer>> signedPermutations = getSigned(permutation);
             for (List<Integer> signedPermutation : signedPermutations) {
                 if (isIncreasing(signedPermutation)) {
-//                    System.out.println(signedPermutation);
+                    System.out.println(signedPermutation);
                     count++;
                 }
             }
@@ -59,9 +64,7 @@ public class IncreasingPermutations {
     }
 
     public static void main(String[] args) throws IOException {
-        int n = 10;
-
-        List<Integer> permutation = IntStream.rangeClosed(1, n).boxed().collect(Collectors.toList());
+        final List<Integer> permutation = IntStream.rangeClosed(1, n).boxed().collect(Collectors.toList());
         generate(n, permutation);
         System.out.println(count);
     }
