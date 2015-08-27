f6ad05f20e05a71eaed55a0d96a811c1b19022f4
Samuel Lee <slee@broadinstitute.org>
2015-08-26 23:35:50 -0400

    III week 5: ColoredEdges and GraphToGenome

--- src/main/java/ucsd/III/ColoredEdges.java
+++ src/main/java/ucsd/III/ColoredEdges.java
@@ -1,41 +1,40 @@
 package ucsd.III;
 
+import org.apache.commons.lang3.StringUtils;
 import ucsd.ConsoleCapturer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ColoredEdges {
-    public static List<Integer> getCycle(List<Integer> chromosome) {
-        List<Integer> cycle = new ArrayList<>(2*chromosome.size());
-        for (int j = 1; j <= chromosome.size(); j++) {
-            int i = chromosome.get(j);
-            if (i > 0) {
-                cycle.set(2*j - 1, 2*i - 1);
-                cycle.set(2*j, 2*i);
-            } else {
-                cycle.set(2*j - 1, -2*i);
-                cycle.set(2*j, -2*i - 1);
+    public static List<Map.Entry<Integer, Integer>> getEdges(List<List<Integer>> genome) {
+        List<Map.Entry<Integer, Integer>> edges = new ArrayList<>();
+        for (List<Integer> chromosome : genome) {
+            List<Integer> cycle = ChromosomeToCycle.getCycle(chromosome);
+            cycle.add(cycle.get(0));
+            for (int j = 0; j < chromosome.size(); j++) {
+                edges.add(new AbstractMap.SimpleEntry<>(cycle.get(2*j + 1), cycle.get(2*j + 2)));
             }
         }
-        return cycle;
+        return edges;
     }
 
     public static String doWork(String dataFileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(dataFileName))) {
 
-            String[] chromosomeStrings = br.readLine().replaceAll("\\(|\\)", "").split("\\s+");
-            List<Integer> chromosome = Arrays.stream(chromosomeStrings).map(Integer::parseInt)
+            String[] genomeStrings = br.readLine().split("\\)\\(");
+            List<List<Integer>> genome = Arrays.stream(genomeStrings)
+                    .map(s -> Arrays.stream(s.replaceAll("\\(|\\)", "").split("\\s+")).map(Integer::parseInt)
+                            .collect(Collectors.toList()))
                     .collect(Collectors.toList());
 
-            List<Integer> cycle = getCycle(chromosome);
+            List<Map.Entry<Integer, Integer>> edges = getEdges(genome);
 
-            String result = "(" + cycle.stream().map(String::valueOf).collect(Collectors.joining(" ")) + ")";
+            String result = edges.stream().map(p -> "(" + p.getKey() + ", " + p.getValue() + ")")
+                    .collect(Collectors.joining(", "));
             return ConsoleCapturer.toString(result);
         } catch (IOException e) {
             throw new RuntimeException("Input file not found.");
@@ -43,7 +42,7 @@ public class ColoredEdges {
     }
 
     public static void main(String[] args) throws IOException {
-        String result = doWork("src/test/resources/III/sample/ChromosomeToCycle.txt");
+        String result = doWork("src/test/resources/III/dataset_8222_7.txt");
         System.out.println(result);
     }
 }
--- src/main/java/ucsd/III/GraphToGenome.java
+++ src/main/java/ucsd/III/GraphToGenome.java
@@ -5,37 +5,57 @@ import ucsd.ConsoleCapturer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GraphToGenome {
-    public static List<Integer> getCycle(List<Integer> chromosome) {
-        List<Integer> cycle = new ArrayList<>(2*chromosome.size());
-        for (int j = 1; j <= chromosome.size(); j++) {
-            int i = chromosome.get(j);
-            if (i > 0) {
-                cycle.set(2*j - 1, 2*i - 1);
-                cycle.set(2*j, 2*i);
+    public static List<List<Integer>> getCycles(List<Map.Entry<Integer, Integer>> edges) {
+        List<List<Integer>> cycles = new ArrayList<>();
+        boolean isNewCycle = true;
+        int firstElementOfCycle = -1;
+        for (Map.Entry<Integer, Integer> edge : edges) {
+            int first = edge.getKey();
+            int second = edge.getValue();
+            if (isNewCycle) {
+                firstElementOfCycle = first;
+                cycles.add(new ArrayList<>());
+                isNewCycle = false;
+            }
+            cycles.get(cycles.size() - 1).add(first);
+            if ((cycles.get(cycles.size() - 1).size() > 1 && (second == firstElementOfCycle + 1 || second == firstElementOfCycle - 1)) ||
+                    (Math.max(first, second) == Math.min(first, second) + 1 && Math.max(first, second) % 2 == 0)) {
+                cycles.get(cycles.size() - 1).add(0, second);
+                isNewCycle = true;
             } else {
-                cycle.set(2*j - 1, -2*i);
-                cycle.set(2*j, -2*i - 1);
+                cycles.get(cycles.size() - 1).add(second);
             }
         }
-        return cycle;
+        return cycles;
+    }
+
+    public static List<List<Integer>> getGenome(List<Map.Entry<Integer, Integer>> edges) {
+        List<List<Integer>> genome = new ArrayList<>();
+        for (List<Integer> cycle : getCycles(edges)) {
+            List<Integer> chromosome = CycleToChromosome.getChromosome(cycle);
+            genome.add(chromosome);
+        }
+        return genome;
     }
 
     public static String doWork(String dataFileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(dataFileName))) {
 
-            String[] chromosomeStrings = br.readLine().replaceAll("\\(|\\)", "").split("\\s+");
-            List<Integer> chromosome = Arrays.stream(chromosomeStrings).map(Integer::parseInt)
+            String[] edgesStrings = br.readLine().split("\\), \\(");
+            List<Map.Entry<Integer, Integer>> edges = Arrays.stream(edgesStrings)
+                    .map(s -> s.replaceAll("\\(|\\)", "").split(", "))
+                    .map(a -> new AbstractMap.SimpleEntry<>(Integer.valueOf(a[0]), Integer.valueOf(a[1])))
                     .collect(Collectors.toList());
 
-            List<Integer> cycle = getCycle(chromosome);
+            List<List<Integer>> genome = getGenome(edges);
 
-            String result = "(" + cycle.stream().map(String::valueOf).collect(Collectors.joining(" ")) + ")";
+            String result = genome.stream()
+                    .map(l -> "(" + l.stream().map(i -> String.format("%+d", i)).collect(Collectors.joining(" ")) + ")")
+                    .collect(Collectors.joining(""));
             return ConsoleCapturer.toString(result);
         } catch (IOException e) {
             throw new RuntimeException("Input file not found.");
@@ -43,7 +63,7 @@ public class GraphToGenome {
     }
 
     public static void main(String[] args) throws IOException {
-        String result = doWork("src/test/resources/III/sample/ChromosomeToCycle.txt");
+        String result = doWork("src/test/resources/III/dataset_8222_8.txt");
         System.out.println(result);
     }
 }
