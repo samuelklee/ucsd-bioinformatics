298eaef37012e12506b44d06ae42ed6629d37924
Samuel Lee <lee.samuel.k@gmail.com>
2015-10-06 23:16:34 -0400

    IV week 1: added tests

---
+++ src/test/java/ucsd/IV/LimbLengthTest.java
@@ -0,0 +1,22 @@
+package ucsd.IV;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ucsd.III.LimbLength;
+
+public class LimbLengthTest {
+    @Test
+    public void test() {
+        String result = LimbLength.doWork("src/test/resources/IV/sample/LimbLength.txt");
+        String expected = "2";
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testExtra() {
+        String result = LimbLength.doWork("src/test/resources/IV/sample/LimbLengthExtra.txt");
+        String expected = "534";
+        Assert.assertEquals(result, expected);
+    }
+}
+
